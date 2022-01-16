8576a9607aa2a2abc1dbeaa5d593703899eb90c3
cross <crosstown11223@yahoo.com>
2022-01-16 10:52:11 -0500

    created method divide

--- src/Ptactice/Methods.java
+++ src/Ptactice/Methods.java
@@ -4,4 +4,9 @@ public class Methods {
     public static void main(String[] args) {
         System.out.println("Hello word");
     }
+
+    public static int divide(int num1, int num2){
+        return num1/num2;
+    }
+
 }
